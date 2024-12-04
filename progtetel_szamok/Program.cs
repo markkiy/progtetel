1fb64fe7af8950932520d741b4cc1411558ed512
zsombee <hornyik.zsombor@ckik.hu>
2024-12-04 11:15:05 +0100

    2. feladat

--- progtetel_szamok/Program.cs
+++ progtetel_szamok/Program.cs
@@ -8,11 +8,21 @@ namespace progtetel_szamok
         static void Main(string[] args)
         {
             int[] x = { 2, 3, 4, 5, 15, 6, 36, 47, 7, 8, 9 };
+           
 
             // 1. párosak ki válogatása
 
             // 2. maximum hely és érték
+            int maxi = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] > x[maxi])
+                {
+                    maxi = i;
+                }
+            }
 
+            Console.WriteLine($"{maxi + 1}, {x[maxi]}");
         }
     }
 }
