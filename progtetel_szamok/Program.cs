36ac9ab740e1295c09c988e0b92c166801a2d791
marki <homoki.mark@ckik.hu>
2024-12-04 11:17:00 +0100

    parosak kivalogatasa

--- progtetel_szamok/Program.cs
+++ progtetel_szamok/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Schema;
 
 
 namespace progtetel_szamok
@@ -10,6 +11,26 @@ namespace progtetel_szamok
             int[] x = { 2, 3, 4, 5, 15, 6, 36, 47, 7, 8, 9 };
 
             // 1. párosak ki válogatása
+            int n = x.Length;
+
+
+            int[] parosak = new int[100];
+            int db = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (x[i] % 2 == 0)
+                {
+                    parosak[db] = x[i];
+                    db++;
+                }
+            }
+
+            for (int i = 0; i < db; i++)
+            {
+                Console.WriteLine(parosak[i]);
+            }
+
 
             // 2. maximum hely és érték
 
